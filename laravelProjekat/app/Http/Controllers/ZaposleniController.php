c615fc53972696f6137989c7bf679b88f4fd64be
jovanab3 <pepyjohny@gmail.com>
2023-12-24 15:35:44 +0100

    crud operacije i kontroler zaposleni

--- laravelProjekat/app/Http/Controllers/ZaposleniController.php
+++ laravelProjekat/app/Http/Controllers/ZaposleniController.php
@@ -6,82 +6,94 @@ use App\Http\Resources\ZaposleniResource;
 use App\Models\User;
 use App\Models\Zaposleni;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class ZaposleniController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index()  
     {
-        return ZaposleniResource::collection(User::all());
+        $sviZaposleni = Zaposleni::all();
+        return  ZaposleniResource::collection($sviZaposleni);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(Request $request)  
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6', 
+            'pozicija' => 'required|string|max:255',
+            'odeljenje' => 'required|string|max:255',
+            'datum_pocetka_rada' => 'required|date',
+            'datum_kraja_ugovora' => 'nullable|date',
+            'plata' => 'required|numeric',
+            'firma_id' => 'required|exists:firmas,id',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $zaposleni = new User;
+        $zaposleni->name = $request->name;
+        $zaposleni->email = $request->email;
+        $zaposleni->password = Hash::make($request->password); 
+        $zaposleni->pozicija = $request->pozicija;
+        $zaposleni->odeljenje = $request->odeljenje;
+        $zaposleni->datum_pocetka_rada = $request->datum_pocetka_rada;
+        $zaposleni->datum_kraja_ugovora = $request->datum_kraja_ugovora;
+        $zaposleni->plata = $request->plata;
+        $zaposleni->firma_id = $request->firma_id;
+        $zaposleni->save();
+
+        return response()->json(['message' => 'Zaposleni uspešno kreiran', 'zaposleni' => $zaposleni], 201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function show($id)  
     {
-        //
+        $zaposleni = User::find($id);
+        if ($zaposleni) {
+            return new ZaposleniResource($zaposleni);
+        }
+        return response()->json(['error' => 'Zaposleni nije pronađen'], 404);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Zaposleni  $zaposleni
-     * @return \Illuminate\Http\Response
-     */
-    public function show( $id)
+    public function update(Request $request, $id) //PUT
     {
-        return new ZaposleniResource(User::find($id));
+        $zaposleni = User::find($id);
+        if ($zaposleni) {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'email' => 'required|email',
+                'password' => 'string|min:6',
+                'pozicija' => 'required|string|max:255',
+                'odeljenje' => 'required|string|max:255',
+                'datum_pocetka_rada' => 'required|date',
+                'datum_kraja_ugovora' => 'nullable|date',
+                'plata' => 'required|numeric',
+                'firma_id' => 'required|exists:firmas,id',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
+            }
+
+            $zaposleni->update($request->all());
+            return response()->json(['message' => 'Zaposleni uspešno ažuriran', 'zaposleni' => new ZaposleniResource($zaposleni)], 200);
+        } else {
+            return response()->json(['error' => 'Zaposleni nije pronađen'], 404);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Zaposleni  $zaposleni
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Zaposleni $zaposleni)
+    public function destroy($id)  
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Zaposleni  $zaposleni
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Zaposleni $zaposleni)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Zaposleni  $zaposleni
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Zaposleni $zaposleni)
-    {
-        //
+        $zaposleni = User::find($id);
+        if ($zaposleni) {
+            $zaposleni->delete();
+            return response()->json(['message' => 'Zaposleni obrisan'], 200);
+        } else {
+            return response()->json(['error' => 'Zaposleni nije pronađen'], 404);
+        }
     }
 }

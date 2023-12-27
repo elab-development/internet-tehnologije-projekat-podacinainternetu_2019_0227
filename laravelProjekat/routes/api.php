790b361b007d4acbba67fb6ca6bcb52e59451ef6
nikolam70 <nikolaynwa1@gmail.com>
2023-12-27 22:50:20 +0100

    zasticene rute

--- laravelProjekat/routes/api.php
+++ laravelProjekat/routes/api.php
@@ -18,25 +18,31 @@ use App\Http\Controllers\FajlController;
 |
 */
 
-//  Route::get('/firme',[FirmaController::class,'index']);
-//  Route::get('/firme/{id}',[FirmaController::class,'show']);
+Route::get('/firme',[FirmaController::class,'index']);
+Route::get('/firme/{id}',[FirmaController::class,'show']);
 
-//  Route::delete('/firme/{id}',[FirmaController::class,'destroy']);
 
-//  Route::post('/firme',[FirmaController::class,'store']);
-//  Route::put('/firme/{id}',[FirmaController::class,'update']);
 Route::post('/register', [AuthController::class, 'register']); 
 Route::post('/login', [AuthController::class, 'login']); 
 
+Route::resource('/task',TaskController::class);
 
 
- Route::resource('/firme',FirmaController::class);
+ //Route::resource('/firme',FirmaController::class);
  
- Route::resource('/zaposleni',ZaposleniController::class);
- Route::resource('/task',TaskController::class);
+ Route::resource('/zaposleni',ZaposleniController::class)->only(['index','show']);
 
-  
+
+ Route::middleware('auth:sanctum')->resource('/zaposleni',ZaposleniController::class)->except(['index','show']);
+
+
+
+ Route::middleware('auth:sanctum')->delete('/firme/{id}',[FirmaController::class,'destroy']);
+ Route::middleware('auth:sanctum')->post('/firme',[FirmaController::class,'store']);
+ Route::middleware('auth:sanctum')->put('/firme/{id}',[FirmaController::class,'update']);
+
  Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
  Route::middleware('auth:sanctum')->get('/fajlovi', [FajlController::class, 'index']); 
  Route::middleware('auth:sanctum')->post('/fajlovi/upload', [FajlController::class, 'upload']);  
  Route::middleware('auth:sanctum')->delete('/fajlovi/delete/{fileId}', [FajlController::class, 'destroy']);
@@ -44,4 +50,4 @@ Route::post('/login', [AuthController::class, 'login']);
 
  
  //Route::get('/fajlovi/download', [FajlController::class, 'download']);
-  ///Route::put('/fajlovi/update/{fileId}', [FajlController::class, 'update']);
+ ///Route::put('/fajlovi/update/{fileId}', [FajlController::class, 'update']);
